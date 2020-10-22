ef7915beccd9398d99335872a57bceaacaad59e6
cgarchbold <cgarchbold@gmail.com>
2020-10-21 21:28:44 -0400

    Building Nueral network class

--- nn.py
+++ nn.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 
-# input any array-like, pass each through sigmoids
 def sigmoid_F(X):
     """
     Sigmoid Function - Preform the sigmoid function on array_like X
@@ -24,28 +23,37 @@ def softmax_F(X):
     X = np.array(X)
     return np.exp(X)/sum(np.exp(X))
 
+def softmaxDerivative_F(X):
+    """
+    Sofmax Derivative Function - Preform the softmax derivative function on array_like X
+    """
+    X = np.array(X)
+    return softmax_F(X) * ( np.identity(X.shape[0]) - softmax_F(X) ) 
 
 class MNIST_dataset():
 
     def __init__(self, csv_path):
+        """
+        MNIST_dataset - (csv_path)
+            - Loads from comma separated values file
+        """
         df = pd.read_csv(csv_path)
         data = df.to_numpy()
 
         self.classes = data[:,0]
-        self.images = data[:,1:]
+        self.images = data[:,1:] / 255.0
 
     def __len__(self):
         return len(self.images)
 
     def out_feat(self):
-        return np.unique(self.classes)
+        return len(np.unique(self.classes))
 
     def in_feat(self):
         return 784
 
     def __getitem__(self,idx):
         return {'image': self.images[idx],'class': self.classes[idx]}
-
 
 # Class to represent a nueral network 
 class NueralNetwork():
@@ -59,7 +67,6 @@ class NueralNetwork():
             weights = np.random.rand( sizes[i+1], sizes[i] )
             self.params.append(weights)
             
-
     def __str__(self):
         return str(self.params)
 
@@ -75,9 +82,13 @@ class NueralNetwork():
         return activation
 
     def backward(self, y, activation):
+        #for each set of weights
+        delta = []
+
         return y # Need to watch lecture
         
-
+    def update(self, something):
+        return something
 
     def predict(self, x):
         return self.forward(x)
@@ -85,7 +96,12 @@ class NueralNetwork():
     def train(self, epochs, dataset):
 
         for epoch in range(epochs):
-            # declare inputs
-            inputs = epoch # FIX
+            
+            # could to random indices batches, OOOOOO NORMAL or gaussian here?
+            for index in range(len(dataset)):
 
-            #forward input 
+                output = self.forward(dataset[index]['image'])
+
+                delta = self.backward(dataset[index]['class'], output)
+
+                self.update(delta)
